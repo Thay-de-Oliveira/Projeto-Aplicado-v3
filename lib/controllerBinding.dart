87bb8c68c2b24c54b0e5967adcea0b3482282ce9
MombachW <william.mombach@gmail.com>
2023-11-22 21:00:01 -0300

    Atualizado controllerBinding

--- lib/controllerBinding.dart
+++ lib/controllerBinding.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 import 'package:projetoaplicado/backend/controllers/acontecimentoController.dart';
 import 'backend/controllers/cidadaoController.dart';
+import 'backend/controllers/atendimentosController.dart';
 
 class ControllerBinding implements Bindings  {
   @override
   void dependencies(){
     Get.lazyPut<CidadaoController>(() => CidadaoController());
     Get.lazyPut<AcontecimentoController>(() => AcontecimentoController());
+    Get.lazyPut<AtendimentoController>(()=> AtendimentoController())
   }
 
 }
