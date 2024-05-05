f61c87dc288e12d0daf8966314f2f0bbc1e44726
Thay-de-Oliveira <thay.l.de.oliveira@gmail.com>
2024-05-04 22:48:34 -0300

    Commit to master
    Alteração na camera

--- lib/app/camera.dart
+++ lib/app/camera.dart
@@ -15,6 +15,13 @@ class _CameraState extends State<Camera> {
   final TextEditingController _controller = TextEditingController();
   XFile? _imageFile;
 
+  @override
+  void initState() {
+    super.initState();
+    // Chamar a função para abrir a câmera automaticamente ao entrar na página
+    _takePicture();
+  }
+
   Future<void> _takePicture() async {
     final XFile? picture = await Navigator.push(
       context,
@@ -27,13 +34,13 @@ class _CameraState extends State<Camera> {
     });
   }
 
-  Future<void> _uploadImage() async {
+  /*Future<void> _uploadImage() async {
     final XFile? image =
         await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
       _imageFile = image;
     });
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -42,44 +49,10 @@ class _CameraState extends State<Camera> {
         appBar: AppBar(
           title: Text('Camera Widget'),
         ),
-        body: TextFormField(
-          controller: _controller,
-          decoration: InputDecoration(
-            labelText: 'Registro da vistoria',
-            labelStyle: const TextStyle(fontSize: 16),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.grey,
-              ),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Colors.blue,
-              ),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            suffixIcon: Padding(
-              padding: const EdgeInsets.only(right: 16.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.camera_alt),
-                    onPressed: _takePicture,
-                  ),
-                  const SizedBox(width: 16),
-                  IconButton(
-                    icon: const Icon(Icons.file_upload),
-                    onPressed: _uploadImage,
-                  ),
-                ],
-              ),
-            ),
-          ),
+        body: Center(
+          // Aqui você pode adicionar qualquer UI adicional, se necessário
+          // Como a tela da câmera já será aberta automaticamente,
+          // Não é necessário ter um campo de texto
         ),
       ),
     );
