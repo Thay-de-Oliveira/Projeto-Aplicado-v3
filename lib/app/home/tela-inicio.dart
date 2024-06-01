13ea227d3aa8ef28bb15312327814a0718701e60
Thay-de-Oliveira <thay.l.de.oliveira@gmail.com>
2024-05-31 23:40:26 -0300

    Pagina inicial responsiva

--- lib/app/home/tela-inicio.dart
+++ lib/app/home/tela-inicio.dart
@@ -1,16 +1,13 @@
-import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:projetoaplicado/app/app-state.dart';
+import 'package:projetoaplicado/app/home/atendimentos/cadastro/tela-atendimento-forms.dart';
 import 'package:projetoaplicado/app/home/atendimentos/historico/tela-atend-hist-detalhes.dart';
 import 'package:projetoaplicado/app/home/cidadaos/tela-cadastro-cidadao.dart';
 import 'package:projetoaplicado/app/home/relatorios/acontecimento/tela-relatorios-acontecimento.dart';
-import 'package:provider/provider.dart';
-
+import 'package:projetoaplicado/app/home/acontecimentos/tela-acontecimentos-forms.dart';
 import '../components/globais/barra-superior.dart';
 import '../components/globais/menu-inferior.dart';
-import 'atendimentos/cadastro/tela-atendimento-forms.dart';
-import 'acontecimentos/tela-acontecimentos-forms.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key, required this.title});
@@ -30,298 +27,56 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     var appState = Provider.of<AppState>(context, listen: false);
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: null,
-      backgroundColor: Colors.grey[200], // Cor de fundo cinza claro
+      backgroundColor: Colors.grey[200],
       body: Stack(
         children: <Widget>[
           SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SizedBox(
-                  height:
-                      80, // Define o espaço de 20 pixels abaixo do Container
+                SizedBox(height: 80),
+                buildMenuCard(
+                  context,
+                  'Acontecimentos',
+                  'assets/imagens/chuva-icone.png',
+                  Colors.lightBlueAccent,
+                  AcontecimentosForms(),
+                  appState,
+                  'acontecimentos',
                 ),
-                Center(
-                  // Acontecimentos
-                  child: InkWell(
-                    onTap: () {
-                      appState.atualizarTela('acontecimentos');
-                      // Navegar para a tela de Acontecimentos
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => AcontecimentosForms(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: 330,
-                      height: 85,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFFEEEEFF)),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        shadows: [
-                          BoxShadow(
-                            color: Color(0x7FB0DCEE),
-                            blurRadius: 8,
-                            offset: Offset(1, 1),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          SizedBox(width: 20),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.lightBlueAccent,
-                              shape: BoxShape.circle,
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/imagens/chuva-icone.png',
-                                width: 30,
-                                height: 30,
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 20),
-                          Text(
-                            'Acontecimentos',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                SizedBox(height: 15),
+                buildMenuCard(
+                  context,
+                  'Atendimentos',
+                  'assets/imagens/fone-icone.png',
+                  Colors.greenAccent,
+                  AtendimentoForms(),
+                  appState,
+                  'atendimento',
                 ),
-                SizedBox(
-                  height:
-                      15, // Define o espaço de 20 pixels abaixo do Container
+                SizedBox(height: 15),
+                buildMenuCard(
+                  context,
+                  'Cadastro de Cidadão',
+                  'assets/imagens/cadastro-icone.png',
+                  Colors.orangeAccent,
+                  CadastroCidadao(),
+                  appState,
+                  'cadastro_cidadao',
                 ),
-                Center(
-                  child: InkWell(
-                    onTap: () {
-                      appState.atualizarTela('atendimento');
-                      // Navegue para a tela de Atendimento
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => AtendimentoForms(),
-                        ),
-                      );
-                    },
-                    // Atendimentos
-                    child: Container(
-                      width: 330,
-                      height: 85,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFFEEEEFF)),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        shadows: [
-                          BoxShadow(
-                            color: Color(0x7FB0DCEE),
-                            blurRadius: 8,
-                            offset: Offset(1, 1),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.start, // Alinha à esquerda
-                        children: <Widget>[
-                          SizedBox(
-                              width:
-                                  20), // Espaço entre o círculo e o Container
-                          Container(
-                            width: 50, // Largura do círculo
-                            height: 50, // Altura do círculo
-                            decoration: BoxDecoration(
-                              color: Colors.greenAccent, // Cor verde
-                              shape: BoxShape.circle, // Formato de círculo
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/imagens/fone-icone.png', // Insira o caminho da imagem
-                                width: 30, // Largura da imagem
-                                height: 30, // Altura da imagem
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ), // Espaço entre o círculo e o Texto
-                          Text(
-                            'Atendimentos',
-                            style: TextStyle(
-                              color: Colors.black, // Cor do texto
-                              fontSize: 16, // Tamanho do texto
-                              fontWeight: FontWeight.bold, // Peso da fonte
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height:
-                      15, // Define o espaço de 20 pixels abaixo do Container
-                ),
-                Center(
-                  child: InkWell(
-                    onTap: () {
-                      appState.atualizarTela('cadastro_cidadao');
-                      // Navegue para a tela de cadastro do cidadão
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => CadastroCidadao(),
-                        ),
-                      );
-                    },
-                    // Cadastro do Cidadão
-                    child: Container(
-                      width: 330,
-                      height: 85,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFFEEEEFF)),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        shadows: [
-                          BoxShadow(
-                            color: Color(0x7FB0DCEE),
-                            blurRadius: 8,
-                            offset: Offset(1, 1),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.start, // Alinha à esquerda
-                        children: <Widget>[
-                          SizedBox(
-                              width:
-                                  20), // Espaço entre o círculo e o Container
-                          Container(
-                            width: 50, // Largura do círculo
-                            height: 50, // Altura do círculo
-                            decoration: BoxDecoration(
-                              color: Colors.orangeAccent, // Cor laranja
-                              shape: BoxShape.circle, // Formato de círculo
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/imagens/cadastro-icone.png', // Insira o caminho da imagem
-                                width: 30, // Largura da imagem
-                                height: 30, // Altura da imagem
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ), // Espaço entre o círculo e o Texto
-                          Text(
-                            'Cadastro de Cidadão',
-                            style: TextStyle(
-                              color: Colors.black, // Cor do texto
-                              fontSize: 16, // Tamanho do texto
-                              fontWeight: FontWeight.bold, // Peso da fonte
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height:
-                      15, // Define o espaço de 20 pixels abaixo do Container
-                ),
-                Center(
-                  // Relatórios
-                  child: InkWell(
-                    onTap: () {
-                      appState.atualizarTela('relatorios');
-                      // Navegue para a tela de relatorios
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => RelatorioAcontecimento(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: 330,
-                      height: 85,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFFEEEEFF)),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        shadows: [
-                          BoxShadow(
-                            color: Color(0x7FB0DCEE),
-                            blurRadius: 8,
-                            offset: Offset(1, 1),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.start, // Alinha à esquerda
-                        children: <Widget>[
-                          SizedBox(
-                              width:
-                                  20), // Espaço entre o círculo e o Container
-                          Container(
-                            width: 50, // Largura do círculo
-                            height: 50, // Altura do círculo
-                            decoration: BoxDecoration(
-                              color: Colors.yellowAccent, // Cor amarelo
-                              shape: BoxShape.circle, // Formato de círculo
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                'assets/imagens/icon-relatorio.png', // Insira o caminho da imagem
-                                width: 30, // Largura da imagem
-                                height: 30, // Altura da imagem
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ), // Espaço entre o círculo e o Texto
-                          Text(
-                            'Relatórios',
-                            style: TextStyle(
-                              color: Colors.black, // Cor do texto
-                              fontSize: 16, // Tamanho do texto
-                              fontWeight: FontWeight.bold, // Peso da fonte
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                SizedBox(height: 15),
+                buildMenuCard(
+                  context,
+                  'Relatórios',
+                  'assets/imagens/icon-relatorio.png',
+                  Colors.yellowAccent,
+                  RelatorioAcontecimento(),
+                  appState,
+                  'relatorios',
                 ),
               ],
             ),
@@ -335,6 +90,74 @@ class _HomeState extends State<Home> {
         ],
       ),
       bottomNavigationBar: MenuInferior(),
+    );
+  }
+
+  Widget buildMenuCard(BuildContext context, String title, String imagePath, Color circleColor, Widget navigateTo, AppState appState, String tela) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Center(
+      child: InkWell(
+        onTap: () {
+          appState.atualizarTela(tela);
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => navigateTo,
+            ),
+          );
+        },
+        child: Container(
+          width: screenWidth * 0.9,
+          height: 85,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(width: 1, color: Color(0xFFEEEEFF)),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            shadows: [
+              BoxShadow(
+                color: Color(0x7FB0DCEE),
+                blurRadius: 8,
+                offset: Offset(1, 1),
+                spreadRadius: 0,
+              )
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(width: 20),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: circleColor,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Image.asset(
+                    imagePath,
+                    width: 30,
+                    height: 30,
+                  ),
+                ),
+              ),
+              SizedBox(width: 20),
+              Flexible(
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
