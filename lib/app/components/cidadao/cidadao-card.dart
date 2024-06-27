cb4399322599aa7ccd90f2061c167d0b5725e830
Thay-de-Oliveira <thay.l.de.oliveira@gmail.com>
2024-06-26 23:32:10 -0300

    Criação de componente "CardCidadao"

---
+++ lib/app/components/cidadao/cidadao-card.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:projetoaplicado/backend/models/cidadaoModel.dart';
+import 'package:projetoaplicado/app/home/cidadaos/historico/tela-detalhes-cidadao.dart';
+
+class CidadaoCard extends StatelessWidget {
+  final CidadaoModel cidadao;
+
+  const CidadaoCard({Key? key, required this.cidadao}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Direcionar para a tela de detalhes do cidadão
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetalhesCidadao(),
+          ),
+        );
+      },
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: const EdgeInsets.fromLTRB(8, 12, 8, 8), // Ajuste aqui o padding superior para aumentar o espaçamento
+            width: constraints.maxWidth,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromARGB(62, 133, 133, 133),
+                  blurRadius: 2, // Aumenta a área de desfoque da sombra
+                  offset: Offset(2, 2), // Ajusta a posição da sombra
+                  spreadRadius: 2, // Aumenta a área de propagação da sombra
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Cidadão: ${cidadao.name}',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700,
+                    height: 0,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'CPF: ${cidadao.cpf}',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.85),
+                    fontSize: 12,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Endereço: ${cidadao.rua}, Bairro ${cidadao.bairro}',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.85),
+                    fontSize: 12,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+                const SizedBox(height: 12), // Aumenta o espaçamento entre o texto e o botão
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    width: 116,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFCFDDF2),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        // Implementar ação de informações
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetalhesCidadao(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: 115,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Informações',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 10,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w600,
+                              height: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/app/home/cidadaos/historico/tela-cidadaos-cadastrados.dart
+++ lib/app/home/cidadaos/historico/tela-cidadaos-cadastrados.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../../../components/globais/barra-superior.dart';
+import '../../../components/globais/menu-inferior.dart';
+import 'package:projetoaplicado/app/components/cidadao/cidadao-card.dart';
 import 'package:projetoaplicado/app/components/globais/filtro-cidadao.dart';
+
 import 'package:projetoaplicado/app/home/cidadaos/cadastro/tela-forms-cidadao.dart';
-import 'package:projetoaplicado/app/home/cidadaos/historico/tela-cidadaos-cadastrados.dart';
-import 'package:projetoaplicado/app/home/cidadaos/historico/tela-detalhes-cidadao.dart';
 import 'package:projetoaplicado/app/home/tela-inicio.dart';
+
+
 import '../../../../backend/models/cidadaoModel.dart';
 import '../../../../backend/controllers/cidadaoController.dart';
 import '../../../../backend/controllers/cepController.dart';
-import '../../../components/globais/barra-superior.dart';
-import '../../../components/globais/menu-inferior.dart';
+
 
 class HistoricoCidadao extends StatefulWidget {
   @override
@@ -147,72 +150,6 @@ class _HistoricoCidadaoState extends State<HistoricoCidadao> {
           ),
         ),
       ],
-    );
-  }
-
-  Widget buildHistoricoItem(String id, String nome, String ultimoAtendimento, String cobrade) {
-    return GestureDetector(
-      onTap: () {
-        // Direcionar para a tela de detalhes do cidadão
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => DetalhesCidadao(),
-          ),
-        );
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 5,
-              spreadRadius: 2,
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              id,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(nome),
-            Text(ultimoAtendimento),
-            Text(cobrade),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Implementar ação de informações
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetalhesCidadao(),
-                    ),
-                  );
-                },
-                child: Text('Informações'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFBBD8F0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
@@ -355,11 +292,20 @@ class _HistoricoCidadaoState extends State<HistoricoCidadao> {
                   ),
                 ),
                 const SizedBox(height: 20),
-                buildHistoricoItem(
-                  'Lucas - 001', //Colocar ID por cidadão?
-                  'Nome completo: Lucas da Silva',
-                  'CPF: ***.***.***-01',
-                  'Bairro: Efapi | Chapecó - SC',
+                CidadaoCard(
+                  cidadao: CidadaoModel(
+                    name: 'Lucas da Silva',
+                    cpf: '12345678901',
+                    rg: '1234567',
+                    cep: '12345678',
+                    rua: 'Rua Jovani de Morais',
+                    bairro: 'Efapi',
+                    cidade: 'Chapecó',
+                    estado: 'SC',
+                    numeroCasa: 100,
+                    numPessoasNaCasa: 4,
+                    telefone: '999999999',
+                  ),
                 ),
               ],
             ),
@@ -367,146 +313,6 @@ class _HistoricoCidadaoState extends State<HistoricoCidadao> {
         ],
       ),
       bottomNavigationBar: MenuInferior(),
-    );
-  }
-}
-
-// FiltroCidadao Widget
-class FiltroCidadao extends StatefulWidget {
-  final List<String> name;
-  final Function(Map<String, dynamic>) onSave;
-
-  FiltroCidadao({required this.name, required this.onSave});
-
-  @override
-  _FiltroCidadaoState createState() => _FiltroCidadaoState();
-}
-
-class _FiltroCidadaoState extends State<FiltroCidadao> {
-  DateTimeRange? selectedDateRange;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Filtros',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Divider(color: Colors.grey),
-              const SizedBox(height: 10),
-              GestureDetector(
-                onTap: () async {
-                  selectedDateRange = await showDateRangePicker(
-                    context: context,
-                    firstDate: DateTime.now().subtract(Duration(days: 30)),
-                    lastDate: DateTime.now(),
-                    builder: (context, child) {
-                      return Theme(
-                        data: Theme.of(context).copyWith(
-                          colorScheme: ColorScheme.light(
-                            primary: Color(0xFF1B7CB3),
-                            onPrimary: Colors.white,
-                            onSurface: Color(0xFF1B7CB3),
-                            surface: const Color.fromARGB(255, 193, 214, 230),
-                          ),
-                          textButtonTheme: TextButtonThemeData(
-                            style: TextButton.styleFrom(
-                              foregroundColor: Color(0xFF1B7CB3),
-                            ),
-                          ),
-                        ),
-                        child: child!,
-                      );
-                    },
-                  );
-                  setState(() {});
-                },
-                child: AbsorbPointer(
-                  child: TextField(
-                    decoration: _customInputDecoration(
-                      selectedDateRange == null
-                          ? 'Selecione período para filtrar (máx 15 dias)'
-                          : 'Período: ${selectedDateRange?.start.toIso8601String().split('T').first} - ${selectedDateRange?.end.toIso8601String().split('T').first}',
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(
-                      'Cancelar',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      Map<String, dynamic> filters = {
-                        'dataInicio': selectedDateRange?.start,
-                        'dataFim': selectedDateRange?.end,
-                      };
-                      widget.onSave(filters);
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(
-                      'Salvar',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  InputDecoration _customInputDecoration(String labelText) {
-    return InputDecoration(
-      labelText: labelText,
-      labelStyle: const TextStyle(fontSize: 16),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.grey),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.blue),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
     );
   }
 }
